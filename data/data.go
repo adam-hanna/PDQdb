ad1e6373e81aea40372ca8d66820cb40cbcfeccb
Jonathan Barronville <jonathan@belairlabs.com>
2015-03-16 02:08:28 -0400

    Fixed copyright.

--- data/data.go
+++ data/data.go
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015-present Adam Hanna
- * Copyright (C) 2015-present Jonathan Barronville
+ * Copyright (C) 2015-present Jonathan Barronville <jonathan@belairlabs.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
